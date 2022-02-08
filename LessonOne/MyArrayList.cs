1672ba24ff5ae96ed3d89604c1f6b075b96855c0
Giocatory <nivolyas@gmail.com>
2022-02-08 11:06:23 +0300

    Use ArrayList in namespace System.Collections

---
+++ LessonOne/MyArrayList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace LessonOne
+{
+    internal class MyArrayList
+    {
+        private ArrayList strArray = new();
+
+        public void AddArrayToArray(params string[] values)
+        {
+            strArray.AddRange(values);
+        }
+        public void AddValue(string value)
+        {
+            strArray.Add(value);
+        }
+        public void ShowArray()
+        {
+            for (int i = 0; i < strArray.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}) {strArray[i]}");
+            }
+        }
+    }
+}
--- LessonOne/Program.cs
+++ LessonOne/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LessonOne;
+/*
+ non-generic collections - System.Collections;
+ generic collections - System.Collections.Generic;
+ */
+
+// ArrayList
+MyArrayList myArray = new();
+myArray.AddArrayToArray("One", "Two", "Three");
+myArray.AddArrayToArray(new string[] { "Four", "Five" });
+myArray.AddValue("Six");
+myArray.ShowArray();
+/*
+1) One
+2) Two
+3) Three
+4) Four
+5) Five
+6) Six
+*/
