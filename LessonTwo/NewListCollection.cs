dfaf6554bfc040441bb95a5d28925058185cb8a8
Giocatory <nivolyas@gmail.com>
2022-02-10 18:31:33 +0300

    work with Collection.Generic

---
+++ LessonTwo/NewListCollection.cs
@@ -0,0 +1,19 @@
+﻿namespace LessonTwo
+{
+    internal class NewListCollection
+    {
+        public static void UseGenerList()
+        {
+            List<Person> people = new()
+            {
+                new Person() { FirstName = "Mikhail", LastName = "SBK", Age = 34 },
+                new Person() { FirstName = "Tanya", LastName = "SBK", Age = 33 },
+            };
+            people.Add(new Person() { FirstName = "Margo", LastName = "SBK", Age = 3 });
+
+            Console.WriteLine($"Created {people.Count} peoples...\n");
+            Console.WriteLine(String.Join("\n", people));
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ LessonTwo/NewStackCollection.cs
@@ -0,0 +1,31 @@
+﻿namespace LessonTwo
+{
+    internal class NewStackCollection
+    {
+        public static void UseGenericStack()
+        {
+            Stack<Person> stackOfPeople = new();
+            stackOfPeople.Push(new Person { FirstName = "Mike", LastName = "cbk", Age = 34 });
+            stackOfPeople.Push(new Person { FirstName = "Tanya", LastName = "cbk", Age = 33 });
+            stackOfPeople.Push(new Person { FirstName = "Margo", LastName = "cbk", Age = 3 });
+
+            // View the top element, push it out and view it again.
+            Console.WriteLine("First person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            Console.WriteLine("\nFirst person is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            Console.WriteLine("\nFirst person item is: {0}", stackOfPeople.Peek());
+            Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+
+            try
+            {
+                Console.WriteLine("\nnFirst person is: {0}", stackOfPeople.Peek());
+                Console.WriteLine("Popped off {0}", stackOfPeople.Pop());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("\nError! {0}\n", ex.Message); // Ошибка! Стек пуст.
+            }
+        }
+    }
+}
