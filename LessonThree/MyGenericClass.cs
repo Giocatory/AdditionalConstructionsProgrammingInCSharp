47ffd752ed7ce4f0f678d478b8e0483db212332b
Giocatory <nivolyas@gmail.com>
2022-02-12 11:14:42 +0300

    Creating specials generic methods

---
+++ LessonThree/MyGenericClass.cs
@@ -0,0 +1,18 @@
+﻿namespace LessonThree
+{
+    internal class MyGenericClass
+    {
+        public static void Swap<T>(ref T a, ref T b) // any type can be used
+        {
+            Console.WriteLine($"Поменять местами два элемента, типа {typeof(T)}");
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void DisplayBaseClass<T>()
+        {
+            Console.WriteLine($"Base class of {typeof(T)}: \"{typeof(T).BaseType}\"");
+        }
+    }
+}
---
+++ LessonThree/Program.cs
@@ -0,0 +1,22 @@
+﻿using LessonThree;
+
+// Creating Special Generic Methods
+int a = 1, b = 2;
+MyGenericClass.Swap<int>(ref a, ref b);
+Console.WriteLine($"a - {a}; b - {b}\n\n");
+/*
+Поменять местами два элемента, типа System.Int32
+a - 2; b - 1
+*/
+
+string sa = "World", sb = "Hello";
+// Swap<string>(ref sa, ref sb);
+MyGenericClass.Swap(ref sa, ref sb);
+Console.WriteLine($"a - {sa}; b - {sb}\n\n");
+/*
+Поменять местами два элемента, типа System.String
+a - Hello; b - World
+*/
+
+MyGenericClass.DisplayBaseClass<string>(); // Base class of System.String: "System.Object"
+MyGenericClass.DisplayBaseClass<int>(); // Base class of System.Int32: "System.ValueType"
