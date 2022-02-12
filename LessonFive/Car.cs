577d4bd41941be19a46e4b3080b117ff09a8aad7
Giocatory <nivolyas@gmail.com>
2022-02-12 14:44:14 +0300

    work with delegate

---
+++ LessonFive/Car.cs
@@ -0,0 +1,35 @@
+﻿namespace LessonFive
+{
+    internal class Car
+    {
+        private int speed = 0;
+        public delegate void ToFast();
+
+        private ToFast? toFast;
+
+        public void Start()
+        {
+            speed = 10;
+            Console.WriteLine($"We begin drive! Speed - {speed}");
+        }
+
+        public void Accelerate()
+        {
+            speed += 20;
+            Console.WriteLine($"Speed - {speed}");
+
+            if (speed > 80)
+            {
+                toFast();
+            }
+        }
+
+        public void Stop()
+        {
+            speed = 0;
+            Console.WriteLine($"We stop! Speed - {speed}");
+        }
+
+        public void RegisterOnToFast(ToFast toFast) => this.toFast = toFast;
+    }
+}
---
+++ LessonFive/Program.cs
@@ -0,0 +1,16 @@
+﻿using LessonFive;
+
+Car car = new();
+car.Start();
+car.RegisterOnToFast(HandleOnToFast);
+
+car.Accelerate();
+car.Accelerate();
+car.Accelerate();
+car.Accelerate();
+
+void HandleOnToFast()
+{
+    Console.WriteLine("Very fast? stop!");
+    car.Stop();
+}
