f45c16da97f5dd0e6109c87c8aa02fbaf8240c1b
Giocatory <nivolyas@gmail.com>
2022-02-12 11:41:11 +0300

    Creating Special Generic Classes and Structures

---
+++ LessonFour/Point.cs
@@ -0,0 +1,40 @@
+﻿namespace LessonFour
+{
+    internal struct Point<T>
+    {
+        // Обобщенные данные состояния,
+        private T xPos;
+
+        private T yPos;
+
+        // Обобщенный конструктор,
+        public Point(T xVal, T yVal)
+        {
+            xPos = xVal;
+            yPos = yVal;
+        }
+
+        // Обобщенные свойства,
+        public T X
+        {
+            get { return xPos; }
+            set { xPos = value; }
+        }
+
+        public T Y
+        {
+            get { return yPos; }
+            set { yPos = value; }
+        }
+
+        public override string ToString() => $"({xPos}; {yPos})";
+
+        // Сбросить поля в стандартные значения
+        // для заданного параметра типа,
+        public void ResetPoint()
+        {
+            xPos = default(T);
+            yPos = default(T);
+        }
+    }
+}
---
+++ LessonFour/Program.cs
@@ -0,0 +1,19 @@
+﻿using LessonFour;
+
+// Creating Special Generic Classes and Structures
+
+// Точка с координатами типа int.
+Point<int> p = new(10, 10);
+
+Console.WriteLine($"p.ToString() = {p}"); // p.ToString() = (10; 10)
+p.ResetPoint();
+Console.WriteLine($"p.ToString() = {p}"); // p.ToString() = (0; 0)
+Console.WriteLine();
+
+// Точка с координатами типа double.
+Point<double> p2 = new(5.4, 3.3);
+
+Console.WriteLine($"p2.ToString() = {p2}"); // p2.ToString() = (5,4; 3,3)
+p2.ResetPoint();
+Console.WriteLine($"p2.ToString() = {p2}"); // p2.ToString() = (0; 0)
+Console.ReadLine();
