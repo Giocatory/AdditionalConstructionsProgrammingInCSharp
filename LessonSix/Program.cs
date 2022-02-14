adc45b6162967ba929a672823a673f3701a7e0eb
Giocatory <nivolyas@gmail.com>
2022-02-14 12:06:12 +0300

    work with delegate

---
+++ LessonSix/Program.cs
@@ -0,0 +1,14 @@
+﻿double num1 = Convert.ToInt32(Console.ReadLine());
+double num2 = Convert.ToInt32(Console.ReadLine());
+string? operation = Console.ReadLine();
+
+Console.WriteLine(
+    operation switch
+    {
+        "+" => num1 + num2,
+        "-" => num1 - num2,
+        "*" => num1 * num2,
+        "/" => num1 / num2,
+        _ => "нет такого оператора"
+    }
+);
