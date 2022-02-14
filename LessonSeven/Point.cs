ff0992a83500b2473cc0fd9da2934d7af8c624b4
Giocatory <nivolyas@gmail.com>
2022-02-14 13:56:10 +0300

    operator overloading

---
+++ LessonSeven/Point.cs
@@ -0,0 +1,21 @@
+﻿namespace LessonSeven
+{
+    internal class Point
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override string ToString() => $"[{x}; {y}]";
+
+        public static Point operator +(Point p1, Point p2) => new(p1.x + p2.x, p1.y + p2.y);
+        public static Point operator -(Point p1, Point p2) => new(p1.x - p2.x, p1.y - p2.y);
+        public static Point operator ++(Point p1) => new(p1.x + 1, p1.y + 1);
+        public static Point operator --(Point p1) => new(p1.x - 1, p1.y - 1);
+    }
+}
---
+++ LessonSeven/Program.cs
@@ -0,0 +1,15 @@
+﻿using LessonSeven;
+
+// перегрузка операторов
+// operator overloading
+Point pointOne = new(3, 10);
+Point pointTwo = new(-1, 15);
+Point pointThree = pointOne + pointTwo;
+
+Console.WriteLine(pointThree); // [2; 25]
+
+pointThree -= pointOne;
+Console.WriteLine(pointThree); // [-1; 15]
+
+Console.WriteLine(++pointOne); // [4; 11]
+Console.WriteLine(--pointTwo); // [-2; 14]
