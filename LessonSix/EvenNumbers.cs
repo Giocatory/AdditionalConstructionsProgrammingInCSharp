3e0998aa511c6d7f5bc042db2a123e9c55544a98
Giocatory <nivolyas@gmail.com>
2022-02-14 13:13:31 +0300

    work with delegates and lambda

---
+++ LessonSix/EvenNumbers.cs
@@ -0,0 +1,33 @@
+﻿namespace LessonSix
+{
+    internal class EvenNumbers
+    {
+        private List<int> numbers = new();
+        public List<int> Numbers { get => numbers; set => numbers = value; }
+
+        public void AddRangeToList(params int[] nums) => numbers.AddRange(nums);
+
+        public void TraditionalDelegateSyntax()
+        {
+            Predicate<int> callback = IsEvenNumber;
+            List<int> evenNumbers = numbers.FindAll(callback);
+            Console.WriteLine("Here is Even numbers:");
+            foreach (int evenNumber in evenNumbers) Console.Write($"{evenNumber}, ");
+            Console.WriteLine();
+        }
+        private bool IsEvenNumber(int i)
+        {
+            return (i % 2 == 0);
+        }
+        public void AnonymousMethodSyntax()
+        {
+            // anonymous delegate call
+            // List<int> OddNumbers = numbers.FindAll(delegate (int numbers) { return numbers % 2 != 0; });
+            // with lambda
+            List<int> OddNumbers = numbers.FindAll(num => num % 2 != 0);
+            Console.WriteLine("Here is Odd numbers:");
+            foreach (int odd in OddNumbers) Console.Write($"{odd}, ");
+            Console.WriteLine();
+        }
+    }
+}
--- LessonSix/Program.cs
+++ LessonSix/Program.cs
@@ -1,14 +1,22 @@
-﻿double num1 = Convert.ToInt32(Console.ReadLine());
-double num2 = Convert.ToInt32(Console.ReadLine());
-string? operation = Console.ReadLine();
+﻿using LessonSix;
 
-Console.WriteLine(
-    operation switch
-    {
-        "+" => num1 + num2,
-        "-" => num1 - num2,
-        "*" => num1 * num2,
-        "/" => num1 / num2,
-        _ => "нет такого оператора"
-    }
-);
+HelloMeaasage myLambda = name => $"{name}! We work with delegates and lambda expressions";
+Console.WriteLine(myLambda.Invoke("Mikhail")); // Mikhail! We work with delegates and lambda expressions
+
+EvenNumbers en = new();
+
+en.AddRangeToList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 161, 71, 68);
+
+en.TraditionalDelegateSyntax();
+/*
+Here is Even numbers:
+2, 4, 6, 8, 10, 12, 14, 68,
+*/
+
+en.AnonymousMethodSyntax();
+/*
+Here is Odd numbers:
+1, 3, 5, 7, 9, 11, 13, 15, 161, 71,
+*/
+
+public delegate string HelloMeaasage(string name);
