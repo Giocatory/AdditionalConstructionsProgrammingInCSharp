af8783e0026f093210f8ac1f87f7d44e98e87847
Giocatory <nivolyas@gmail.com>
2022-02-14 12:06:35 +0300

    work with delegate

--- LessonFive/Program.cs
+++ LessonFive/Program.cs
@@ -6,11 +6,15 @@ car.RegisterOnToFast(HandleOnToFast);
 
 car.Accelerate();
 car.Accelerate();
-car.Accelerate();
-car.Accelerate();
+
+MyDelegate myDelegate = car.Accelerate;
+myDelegate += car.Accelerate;
+myDelegate.Invoke();
 
 void HandleOnToFast()
 {
     Console.WriteLine("Very fast? stop!");
     car.Stop();
 }
+
+public delegate void MyDelegate();
