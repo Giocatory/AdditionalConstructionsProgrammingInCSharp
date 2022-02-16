ba8608fc39e389bc942e6806d543a9fbb65b18ec
Giocatory <nivolyas@gmail.com>
2022-02-16 12:43:10 +0300

    experiment with anonymous type

---
+++ LessonSeven(AnonymousType)/Program.cs
@@ -0,0 +1,13 @@
+﻿var purchaseItem = new
+{
+    TimeBought = DateTime.Now,
+    ItemBought = new
+    {
+        Color = "Red",
+        Make = "Saab",
+        CurrentSpeed = 55
+    }
+};
+Console.WriteLine(purchaseItem);
+Console.WriteLine(purchaseItem.TimeBought);
+Console.WriteLine(purchaseItem.ItemBought.Color);
