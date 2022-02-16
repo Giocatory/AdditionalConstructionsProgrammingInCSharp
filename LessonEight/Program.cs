b94181202600351fcf9f81ac695db5537addb353
Giocatory <nivolyas@gmail.com>
2022-02-16 13:37:24 +0300

    base work with LINQ

--- LessonEight/Program.cs
+++ LessonEight/Program.cs
@@ -12,10 +12,28 @@
 Этот аспект делает возможной параллельную обработ­ку данных, возвращаемых из запроса LINQ.
 */
 
+// var query = from g in SomeCollection where g.Contains(" ") orderby g select g;
+Console.WriteLine("Games with \" \"");
 QueryOverStrings();
 /*
+Games with " "
+***** Info about your query using Query Expression *****
+resultSet is of type: OrderedEnumerable`2       <-
+resultSet location: System.Linq
 Fallout 3
 System Shok 2
+*/
+
+// var query = SomeCollection.Where(lambda).OrderBy(lambda).Select(lambda);
+Console.WriteLine("Games start with \"D\" or \"M\"");
+QueryOverStnngsWithExtensionMethods();
+/*
+Games start with "D" or "M"
+***** Info about your query using Query Expression *****
+resultSet is of type: SelectIPartitionIterator`2        <-
+resultSet location: System.Linq
+Diablo
+Morrowwind
 */
 
 static void QueryOverStrings()
@@ -24,5 +42,28 @@ static void QueryOverStrings()
     // LINQ query
     // var == IEnumerable<string>
     var subset = from g in currentVideoGames where g.Contains(" ") orderby g select g;
+
+    ReflectOverQueryResult(subset);
+
+    subset.ToArray();
     foreach (var video in subset) { Console.WriteLine(video); }
+}
+
+static void QueryOverStnngsWithExtensionMethods()
+{
+    string[] currentVideoGames = { "Diablo", "Fallout 3", "Morrowwind", "System Shok 2" };
+    // or query expression
+    var subsetTwo = currentVideoGames.Where(g => g.StartsWith("D") || g.StartsWith("M")).OrderBy(g => g).Select(g => g);
+
+    ReflectOverQueryResult(subsetTwo);
+
+    subsetTwo.ToList<string>();
+    foreach (var video in subsetTwo) { Console.WriteLine(video); }
+}
+
+static void ReflectOverQueryResult(object resultSet, string queryType = "Query Expression")
+{
+    Console.WriteLine($"***** Info about your query using {queryType} *****");
+    Console.WriteLine($"resultSet is of type: {resultSet.GetType().Name}");
+    Console.WriteLine($"resultSet location: {resultSet.GetType().Assembly.GetName().Name}");
 }
