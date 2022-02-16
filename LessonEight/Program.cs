99299625d7cf4b7e8b6a69da11504d5ce4c30979
Giocatory <nivolyas@gmail.com>
2022-02-16 13:06:26 +0300

    base work with LINQ

--- LessonEight/Program.cs
+++ LessonEight/Program.cs
@@ -1,1 +1,28 @@
-﻿//LINQ
+﻿// LINQ
+/*
+•  LINQ to Objects.
+Этот термин относится к действию по применению запросов LINQ к массивам и коллекциям.
+•  LINQ to XML.
+Этот термин относится к действию по использованию LINQ для ма­нипулирования и запрашивания документов XML.
+•  LINQ to DataSet.
+Этот термин относится к действию по применению запросов LINQ к объектам DataSet из ADO.NET.
+•  LINQ to Entities.
+Этот аспект LINQ позволяет использовать запросы LINQ внутри API-интерфейса ADO.NET Entity Framework (EF).
+•  Parallel LINQ(он же PLINQ).
+Этот аспект делает возможной параллельную обработ­ку данных, возвращаемых из запроса LINQ.
+*/
+
+QueryOverStrings();
+/*
+Fallout 3
+System Shok 2
+*/
+
+static void QueryOverStrings()
+{
+    string[] currentVideoGames = { "Diablo", "Fallout 3", "Morrowwind", "System Shok 2" };
+    // LINQ query
+    // var == IEnumerable<string>
+    var subset = from g in currentVideoGames where g.Contains(" ") orderby g select g;
+    foreach (var video in subset) { Console.WriteLine(video); }
+}
