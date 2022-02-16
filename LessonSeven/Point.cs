7e89c19a9478be02789f545a88f7fa4c0bfbad29
Giocatory <nivolyas@gmail.com>
2022-02-16 12:29:04 +0300

    review code

--- LessonSeven/Point.cs
+++ LessonSeven/Point.cs
@@ -14,8 +14,11 @@
         public override string ToString() => $"[{x}; {y}]";
 
         public static Point operator +(Point p1, Point p2) => new(p1.x + p2.x, p1.y + p2.y);
+
         public static Point operator -(Point p1, Point p2) => new(p1.x - p2.x, p1.y - p2.y);
+
         public static Point operator ++(Point p1) => new(p1.x + 1, p1.y + 1);
+
         public static Point operator --(Point p1) => new(p1.x - 1, p1.y - 1);
     }
 }
