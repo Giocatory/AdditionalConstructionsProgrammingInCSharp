f06feb2caccf9296112a332a1aff4ebbe8469277
Giocatory <nivolyas@gmail.com>
2022-02-18 18:13:39 +0300

    return LINQ result

---
+++ LessonEight(Three)/Program.cs
@@ -0,0 +1,16 @@
+﻿// Returning LINQ Results
+
+IEnumerable<string> subset = GetStringSubset();
+foreach (var sub in subset) { Console.WriteLine(sub); }
+/*
+Light Red
+Dark Red
+Red
+*/
+
+static IEnumerable<string> GetStringSubset()
+{
+    string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+
+    return (from c in colors where c.Contains("Red") select c);
+}
