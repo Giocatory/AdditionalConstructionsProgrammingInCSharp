2a418e29721a863f57616766c4464aa5938eb492
Giocatory <nivolyas@gmail.com>
2022-02-18 18:17:48 +0300

    return LINQ results

--- LessonEight(Three)/Program.cs
+++ LessonEight(Three)/Program.cs
@@ -8,9 +8,23 @@ Dark Red
 Red
 */
 
+List<string> subset2 = GetStringsListSubset();
+foreach (var sub2 in subset2) { Console.WriteLine(sub2); }
+/*
+Dark Yellow
+Dark Red
+*/
+
 static IEnumerable<string> GetStringSubset()
 {
-    string[] colors = { "Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple" };
+    string[] colors = { "Light Red", "Green", "Dark Yellow", "Dark Red", "Red", "Purple" };
 
     return (from c in colors where c.Contains("Red") select c);
+}
+
+static List<string> GetStringsListSubset()
+{
+    string[] colors = { "Light Red", "Green", "Dark Yellow", "Dark Red", "Red", "Purple" };
+
+    return (from c in colors where c.Contains("Dark") select c).ToList<string>();
 }
