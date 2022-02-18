dcd33b328739101b8328dc05a5e1335c5069ace9
Giocatory <nivolyas@gmail.com>
2022-02-18 18:58:07 +0300

    Filtering data using the OfType<T> method

---
+++ LessonEight(Five)/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+
+// Filtering data using the OfType<T> method
+
+ArrayList myStuff = new();
+myStuff.AddRange(new object[] { 10, 40, false, "any string", true });
+
+int[] myInts = myStuff.OfType<int>().ToArray();
+foreach (var i in myInts) { Console.WriteLine(i); }
+// 10
+// 40
